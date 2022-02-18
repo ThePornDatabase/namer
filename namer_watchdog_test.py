14a2c7d705b23eadf768c645d7e6827284700284
4c0d3r <nope>
2022-02-17 20:58:36 -0800

    test event listeners

--- namer_watchdog_test.py
+++ namer_watchdog_test.py
@@ -7,6 +7,7 @@ from unittest.mock import patch
 import logging
 import tempfile
 import shutil
+from watchdog.events import FileSystemEvent, FileSystemMovedEvent
 from mutagen.mp4 import MP4
 from namer_dirscanner_test import prepare_workdir
 from namer_mutagen_test import validate_mp4_tags
@@ -55,6 +56,41 @@ class UnitTestAsTheDefaultExecution(unittest.TestCase):
             self.assertEqual(output2.get('\xa9nam'), ['Carmela Clutch: Fabulous Anal 3-Way!'])
 
 
+
+    @patch('namer_metadataapi.__get_response_json_object')
+    @patch('namer.get_poster')
+    def test_eventlisterner_success(self, mock_poster, mock_response):
+        """
+        Test the handle function works for a directory.
+        """
+        with tempfile.TemporaryDirectory(prefix="test") as tmpdir:
+            tempdir = Path(tmpdir)
+
+            config = default_config()
+            config.prefer_dir_name_if_available = True
+            config.watch_dir = tempdir / 'watch'
+            config.watch_dir.mkdir()
+            config.work_dir = tempdir / 'work'
+            config.work_dir.mkdir()
+            config.dest_dir = tempdir / 'dest'
+            config.dest_dir.mkdir()
+            config.failed_dir = tempdir / 'failed'
+            config.failed_dir.mkdir()
+            config.min_file_size=0
+            targets = [new_ea(config.watch_dir)]
+            prepare(targets, mock_poster, mock_response)
+
+            moviehandler = MovieEventHandler(config)
+            moviehandler.on_created(FileSystemEvent(Path(targets[0].file)))
+
+            self.assertFalse(targets[0].file.exists())
+            self.assertEqual(len(list(config.work_dir.iterdir())),0)
+            outputfile = ( config.dest_dir / 'EvilAngel - 2022-01-03 - Carmela Clutch Fabulous Anal 3-Way!' /
+                'EvilAngel - 2022-01-03 - Carmela Clutch Fabulous Anal 3-Way!.mp4')
+            output = MP4(outputfile)
+            self.assertEqual(output.get('\xa9nam'), ['Carmela Clutch: Fabulous Anal 3-Way!'])
+
+
     @patch('namer_metadataapi.__get_response_json_object')
     @patch('namer.get_poster')
     def test_handler_deeply_nested_success(self, mock_poster, mock_response):
@@ -90,8 +126,6 @@ class UnitTestAsTheDefaultExecution(unittest.TestCase):
             outputfile2 = ( config.dest_dir / 'EvilAngel - 2022-01-03 - Carmela Clutch Fabulous Anal 3-Way!' /
                 'EvilAngel - 2022-01-03 - Carmela Clutch Fabulous Anal 3-Way!(1).mp4')
             validate_mp4_tags(self, outputfile2)
-
-
 
     @patch('namer_metadataapi.__get_response_json_object')
     @patch('namer.get_poster')
