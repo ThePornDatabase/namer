db9c19c1f6bb46cfb2600734692ebd55b756ef93
4c0d3r <nope>
2022-04-13 09:03:22 -0700

    docker build sh

--- docker_build.sh
+++ docker_build.sh
@@ -1,10 +1,12 @@
 #!/bin/bash
 BUILD_DATE=$(date -u +'%Y-%m-%dT%H:%M:%SZ')
 CLEAN=$(git diff-index --quiet HEAD; echo $?)
-PROJECT_VERSION=$(poetry version -s)
-#if [[ "${CLEAN}" == "0" ]]; then
-  GIT_HASH=$(git rev-parse --verify HEAD)
-  docker build . --build-arg "BUILD_DATE=${BUILD_DATE}" --build-arg "PROJECT_VERSION=${PROJECT_VERSION}"  --build-arg "GIT_HASH=${GIT_HASH}" -t porndb/namer
-#else
-#  echo Not building for dirty code.
-#fi
+version=$(cat pyproject.toml | grep "version = " | sed 's/.* = //' | sed 's/"//g')
+if [[ "${CLEAN}" == "0" ]]; then
+  export BUILD_DATE=$(date -u +'%Y-%m-%dT%H:%M:%SZ')
+  export GIT_HASH=$(git rev-parse --verify HEAD)
+  export repo=theporndatabase
+  docker build . --build-arg "BUILD_DATE=${BUILD_DATE}" --build-arg "GIT_HASH=${GIT_HASH}" --build-arg "PROJECT_VERSION=${version}" -t "ghcr.io/${repo}/namer:${version}" -t "ghcr.io/${repo}/namer:latest"
+else
+  echo Not building for dirty code.
+fi
