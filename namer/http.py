5088e364ae13c91e9deb80aff00d53103c322072
DirtyRacer1337 <DirtyRacer@ya.ru>
2022-06-20 14:39:01 +0700

    Make compatible with requests style

--- namer/http.py
+++ namer/http.py
@@ -6,20 +6,23 @@ from requests_cache import CachedSession
 
 class Http:
     @staticmethod
-    def request(method, url, cache_session: Optional[CachedSession] = None, **kwargs):
+    def request(method, url, **kwargs):
+        cache_session: Optional[CachedSession] = kwargs.get('cache_session')
+        del kwargs['cache_session']
+
         if kwargs.get("stream", False) or cache_session is None:
             return requests.request(method, url, **kwargs)
         else:
             return cache_session.request(method, url, **kwargs)
 
     @staticmethod
-    def get(url: str, cache_session: Optional[CachedSession] = None, **kwargs):
-        return Http.request("GET", url, cache_session, **kwargs)
+    def get(url: str, **kwargs):
+        return Http.request("GET", url, **kwargs)
 
     @staticmethod
-    def post(url: str, cache_session: Optional[CachedSession] = None, **kwargs):
-        return Http.request("POST", url, cache_session, **kwargs)
+    def post(url: str, **kwargs):
+        return Http.request("POST", url, **kwargs)
 
     @staticmethod
-    def head(url: str, cache_session: Optional[CachedSession] = None, **kwargs):
-        return Http.request("HEAD", url, cache_session, **kwargs)
+    def head(url: str, **kwargs):
+        return Http.request("HEAD", url, **kwargs)
