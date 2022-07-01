63db89d04ae4f5591ecbf0d9e1aca14e5a05aca7
DirtyRacer1337 <DirtyRacer@ya.ru>
2022-07-01 15:24:44 +0700

    Remove debug and add missing mimes

--- namer/web/server.py
+++ namer/web/server.py
@@ -1,6 +1,7 @@
 """
 A wrapper allowing shutdown of a Flask server.
 """
+import mimetypes
 from queue import Queue
 from threading import Thread
 from typing import Optional, Union
@@ -24,34 +25,41 @@ class WebServer:
     __server: Union[MultiSocketServer, BaseWSGIServer]
     __thread: Thread
     __config: NamerConfig
-    __debug: bool
     __command_queue: Queue
 
-    def __init__(self, config: NamerConfig, command_queue: Queue, debug: bool = False):
+    __mime_types: dict = {
+        'font/woff': '.woff',
+        'font/woff2': '.woff2',
+    }
+
+    def __init__(self, config: NamerConfig, command_queue: Queue):
         self.__config = config
-        self.__debug = debug
         self.__command_queue = command_queue
+        self.__add_mime_types()
         self.__make_server()
 
     def __make_server(self):
         path = '/' if self.__config.web_root is None else self.__config.web_root
         blueprint = get_web_routes(self.__config, self.__command_queue)
         app.register_blueprint(blueprint, url_prefix=path, root_path=path)
-        if not self.__debug:
-            compress.init_app(app)
-            self.__server = create_server(app, host=self.__config.host, port=self.__config.port)
-            self.__thread = Thread(target=self.run, daemon=True)
+        compress.init_app(app)
+        self.__server = create_server(app, host=self.__config.host, port=self.__config.port)
+        self.__thread = Thread(target=self.__run, daemon=True)
+
+    def __add_mime_types(self):
+        for mime, ext in self.__mime_types.items():
+            test_mime, test_ext = mimetypes.guess_type(f'0{ext}')
+            if test_mime is None:
+                mimetypes.add_type(mime, ext)
 
     def start(self):
         self.__thread.start()
 
-    def run(self):
+    def __run(self):
         """
         Start server on existing thread.
         """
-        if self.__debug:
-            app.run(debug=True, host=self.__config.host, port=self.__config.port)
-        else:
+        if self.__server:
             self.__server.run()
 
     def stop(self):
