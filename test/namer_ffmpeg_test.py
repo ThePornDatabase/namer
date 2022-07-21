6ad7f36796c5005df1f084315d45d05e2a8fd1d1
4c0d3r <nope>
2022-07-20 22:11:07 -0700

    ffprobe with typed results

--- test/namer_ffmpeg_test.py
+++ test/namer_ffmpeg_test.py
@@ -6,7 +6,7 @@ import tempfile
 import unittest
 from pathlib import Path
 
-from namer.ffmpeg import get_audio_stream_for_lang, get_resolution, update_audio_stream_if_needed
+from namer.ffmpeg import get_audio_stream_for_lang, get_resolution, update_audio_stream_if_needed, ffprobe
 
 
 class UnitTestAsTheDefaultExecution(unittest.TestCase):
@@ -37,6 +37,35 @@ class UnitTestAsTheDefaultExecution(unittest.TestCase):
             self.assertEqual(stream_number, -1)
             stream_number = get_audio_stream_for_lang(file, "eng")
             self.assertEqual(stream_number, -1)
+
+    def test_ffprobe(self) -> None:
+        """
+        read stream info.
+        """
+        with tempfile.TemporaryDirectory(prefix="test") as tmpdir:
+            tempdir = Path(tmpdir)
+            shutil.copytree(Path(__file__).resolve().parent, tempdir / "test")
+            file = tempdir / "test" / "Site.22.01.01.painful.pun.XXX.720p.xpost_wrong.mp4"
+            results = ffprobe(file)
+            self.assertIsNotNone(results)
+            if results:
+                self.assertTrue(results.all_streams()[0].is_video())
+                self.assertEqual(results.all_streams()[0].bit_rate, 8487)
+                self.assertEqual(results.all_streams()[0].height, 240)
+                self.assertEqual(results.all_streams()[0].width, 320)
+                self.assertEqual(results.all_streams()[0].avg_frame_rate, 15.0)
+                self.assertEqual(results.all_streams()[0].codec_name, "h264")
+                self.assertGreaterEqual(results.all_streams()[0].duration, 30)
+                self.assertTrue(results.all_streams()[1].is_audio())
+                self.assertEqual(results.all_streams()[1].disposition_default, True)
+                self.assertEqual(results.all_streams()[1].tags_language, 'und')
+                self.assertTrue(results.all_streams()[2].is_audio())
+                self.assertEqual(results.all_streams()[2].disposition_default, False)
+                self.assertEqual(results.all_streams()[2].tags_language, 'eng')
+                self.assertEqual(results.get_default_video_stream(), results.all_streams()[0])
+                self.assertEqual(results.get_default_audio_stream(), results.all_streams()[1])
+                self.assertEqual(results.get_audio_stream('eng'), results.all_streams()[2])
+                self.assertEqual(results.get_audio_stream('und'), results.all_streams()[1])
 
     def test_update_audio_stream(self):
         """
