3a94a1018e850ef82207ca8a2307b7005746e0c0
4c0d3r <nope>
2022-10-02 17:20:36 -0700

    clean up my own messes

--- namer/web/actions.py
+++ namer/web/actions.py
@@ -133,13 +133,9 @@ def read_failed_log_file(name: str, config: NamerConfig) -> Optional[ComparisonR
     file = config.failed_dir / name
     file = file.parent / (file.stem + '_namer.json.gz')
 
-    print("Log file name: {}", file)
-
     res: Optional[ComparisonResults] = None
     if file.is_file():
         res = _read_failed_log_file(file, file.stat().st_size, file.stat().st_mtime)
-
-    print("Res file {}", res)
 
     return res
 
