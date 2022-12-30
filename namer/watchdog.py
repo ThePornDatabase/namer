d2ed87e96cfec0fb46073399ed380367e20cc19c
DirtyRacer1337 <DirtyRacer@ya.ru>
2022-12-30 08:27:36 +0700

    Fix encoding

--- namer/watchdog.py
+++ namer/watchdog.py
@@ -260,6 +260,7 @@ def create_watcher(namer_watchdog_config: NamerConfig) -> MovieWatcher:
     """
     Configure and start a watchdog looking for new Movies.
     """
+    sys.stdout.reconfigure(encoding='cp1251', errors='backslashreplace')
     logger.remove()
     level = 'DEBUG' if namer_watchdog_config.debug else 'INFO'
     logger.add(sys.stdout, format="<green>{time:YYYY-MM-DD HH:mm:ss}</green> | <level>{level.icon} {level: <8}</level> | {message}", level=level, diagnose=namer_watchdog_config.diagnose_errors)
