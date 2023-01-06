b8570c75de5fa48b0161e9c151804add217f490d
DirtyRacer1337 <DirtyRacer@ya.ru>
2023-01-06 15:16:08 +0700

    Fix sending phash from ui

--- namer/namer.py
+++ namer/namer.py
@@ -194,16 +194,19 @@ def process_file(command: Command) -> Optional[Command]:
             if ffprobe_results:
                 new_metadata.resolution = ffprobe_results.get_resolution()
 
-            target = move_to_final_location(command, new_metadata)
-            tag_in_place(target.target_movie_file, command.config, new_metadata, ffprobe_results)
-            add_extra_artifacts(target.target_movie_file, new_metadata, search_results, phash, command.config)
+            if command.config.send_phash:
+                if not matched or not matched.phash_match:
+                    phash = command.config.vph.get_hashes(command.target_movie_file) if not phash else phash
+
+                    share_phash(new_metadata, phash, command.config)
+                    share_oshash(new_metadata, phash, command.config)
 
             if command.config.mark_collected and not new_metadata.is_collected and new_metadata.type == SceneType.SCENE:
                 toggle_collected(new_metadata, command.config)
 
-            if command.config.send_phash and phash and (not matched or not matched.phash_match):
-                share_phash(new_metadata, phash, command.config)
-                share_oshash(new_metadata, phash, command.config)
+            target = move_to_final_location(command, new_metadata)
+            tag_in_place(target.target_movie_file, command.config, new_metadata, ffprobe_results)
+            add_extra_artifacts(target.target_movie_file, new_metadata, search_results, phash, command.config)
 
             logger.success("Done processing file: {}, moved to {}", command.target_movie_file, target.target_movie_file)
             return target
