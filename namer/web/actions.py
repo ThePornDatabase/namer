548f729db82f434f31b4fd37f8b8730f8e5133ef
DirtyRacer1337 <DirtyRacer@ya.ru>
2023-01-17 03:30:05 +0700

    Add JAV support to UI

--- namer/web/actions.py
+++ namer/web/actions.py
@@ -25,6 +25,7 @@ class SearchType(str, Enum):
     ANY = 'Any'
     SCENES = 'Scenes'
     MOVIES = 'Movies'
+    JAV = 'JAV'
 
 
 def has_no_empty_params(rule: Rule) -> bool:
@@ -82,13 +83,15 @@ def get_search_results(query: str, search_type: SearchType, file: str, config: N
 
     responses = {}
     if search_type == SearchType.ANY or search_type == SearchType.SCENES:
-        # scenes
         url = __build_url(config, name=query, page=page, scene_type=SceneType.SCENE)
         responses[url] = __get_response_json_object(url, config)
 
     if search_type == SearchType.ANY or search_type == SearchType.MOVIES:
-        # movies
         url = __build_url(config, name=query, page=page, scene_type=SceneType.MOVIE)
+        responses[url] = __get_response_json_object(url, config)
+
+    if search_type == SearchType.ANY or search_type == SearchType.JAV:
+        url = __build_url(config, name=query, page=page, scene_type=SceneType.JAV)
         responses[url] = __get_response_json_object(url, config)
 
     file_infos = []
