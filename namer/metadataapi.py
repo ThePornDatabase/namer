2eef836d1ff086f88ebcfd9b50645c1f7c10e419
DirtyRacer1337 <DirtyRacer@ya.ru>
2023-02-05 09:20:12 +0700

    Don't make requests for network

--- namer/metadataapi.py
+++ namer/metadataapi.py
@@ -416,8 +416,7 @@ def __json_to_fileinfo(data, url: str, json_response: str, name_parts: Optional[
         file_info.is_collected = data.is_collected
 
     if data.site.network_id and data.site.network_id != data.site.id:
-        network_name = get_site_name(data.site.network_id, config)
-        file_info.network = network_name
+        file_info.network = data.site.network.name
 
     return file_info
 
@@ -498,20 +497,6 @@ def __get_metadataapi_net_fileinfo(name_parts: Optional[FileInfo], namer_config:
             return file_infos
 
     return []
-
-
-@logger.catch
-def get_site_name(site_id: str, namer_config: NamerConfig) -> Optional[str]:
-    site = None
-
-    url = f"{namer_config.override_tpdb_address}/sites/{site_id}"
-    json_response = __get_response_json_object(url, namer_config)
-
-    if json_response and json_response.strip() != "":
-        json_obj = json.loads(json_response, object_hook=lambda d: SimpleNamespace(**d))
-        site = json_obj.data.name
-
-    return site
 
 
 def get_complete_metadataapi_net_fileinfo(name_parts: Optional[FileInfo], uuid: str, namer_config: NamerConfig) -> Optional[LookedUpFileInfo]:
