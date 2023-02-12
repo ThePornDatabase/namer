fe3d68bccc6cfdb64fdef5d3f920dc1847b93cc2
DirtyRacer1337 <DirtyRacer@ya.ru>
2023-02-12 10:25:02 +0700

    Add husky and lint staged (#180)

--- src/js/main.js
+++ src/js/main.js
@@ -1,11 +1,12 @@
 // noinspection ES6UnusedImports
-import {Popover, Modal} from 'bootstrap'
+// eslint-disable-next-line no-unused-vars
+import { Popover, Modal } from 'bootstrap'
 
 import $ from 'jquery'
 import 'datatables.net-bs5'
-import {escape} from 'lodash'
+import { escape } from 'lodash'
 
-import {Helpers} from './helpers'
+import { Helpers } from './helpers'
 import './themes'
 
 window.jQuery = $
@@ -28,124 +29,124 @@ const deleteButton = $('#deleteButton')
 let modalButton
 
 searchButton.on('click', function () {
-    resultForm.html(Helpers.getProgressBar())
+  resultForm.html(Helpers.getProgressBar())
 
-    const data = {
-        'query': queryInput.val(),
-        'file': queryInput.data('file'),
-        'type': queryType.val(),
-    }
+  const data = {
+    query: queryInput.val(),
+    file: queryInput.data('file'),
+    type: queryType.val()
+  }
 
-    const title = escape(`(${data['file']}) [${data['query']}]`)
-    resultFormTitle.html(title)
-    resultFormTitle.attr('title', title)
+  const title = escape(`(${data.file}) [${data.query}]`)
+  resultFormTitle.html(title)
+  resultFormTitle.attr('title', title)
 
-    Helpers.request('./api/v1/get_search', data, function (data) {
-        Helpers.render('searchResults', data, resultForm, function (selector) {
-            Helpers.initTooltips(selector)
-        })
+  Helpers.request('./api/v1/get_search', data, function (data) {
+    Helpers.render('searchResults', data, resultForm, function (selector) {
+      Helpers.initTooltips(selector)
     })
+  })
 })
 
 phashButton.on('click', function () {
-    resultForm.html(Helpers.getProgressBar())
+  resultForm.html(Helpers.getProgressBar())
 
-    const data = {
-        'file': queryInput.data('file'),
-        'type': queryType.val(),
-    }
+  const data = {
+    file: queryInput.data('file'),
+    type: queryType.val()
+  }
 
-    let title = escape(`(${data['file']})`)
-    resultFormTitle.html(title)
-    resultFormTitle.attr('title', title)
+  const title = escape(`(${data.file})`)
+  resultFormTitle.html(title)
+  resultFormTitle.attr('title', title)
 
-    Helpers.request('./api/v1/get_phash', data, function (data) {
-        Helpers.render('searchResults', data, resultForm, function (selector) {
-            Helpers.initTooltips(selector)
-        })
+  Helpers.request('./api/v1/get_phash', data, function (data) {
+    Helpers.render('searchResults', data, resultForm, function (selector) {
+      Helpers.initTooltips(selector)
     })
+  })
 })
 
 queryInput.on('keyup', function (e) {
-    if (e.which === 13) {
-        searchButton.click()
-    }
+  if (e.which === 13) {
+    searchButton.click()
+  }
 })
 
 filesResult.on('click', '.match', function () {
-    modalButton = $(this)
-    const query = modalButton.data('query')
-    const file = modalButton.data('file')
-    queryInput.val(query)
-    queryInput.data('file', file)
+  modalButton = $(this)
+  const query = modalButton.data('query')
+  const file = modalButton.data('file')
+  queryInput.val(query)
+  queryInput.data('file', file)
 })
 
 filesResult.on('click', '.log', function () {
-    logForm.html(Helpers.getProgressBar())
-    modalButton = $(this)
-    const data = {
-        'file': modalButton.data('file'),
-    }
+  logForm.html(Helpers.getProgressBar())
+  modalButton = $(this)
+  const data = {
+    file: modalButton.data('file')
+  }
 
-    const title = escape(`[${data['file']}]`)
+  const title = escape(`[${data.file}]`)
 
-    logFormTitle.html(title)
-    logFormTitle.attr('title', title)
+  logFormTitle.html(title)
+  logFormTitle.attr('title', title)
 
-    Helpers.request('./api/v1/read_failed_log', data, function (data) {
-        Helpers.render('logFile', data, logForm, function (selector) {
-            Helpers.initTooltips(selector)
-        })
+  Helpers.request('./api/v1/read_failed_log', data, function (data) {
+    Helpers.render('logFile', data, logForm, function (selector) {
+      Helpers.initTooltips(selector)
     })
+  })
 })
 
 filesResult.on('click', '.delete', function () {
-    modalButton = $(this)
-    const file = modalButton.data('file')
-    deleteFile.val(file)
-    deleteFile.data('file', file)
+  modalButton = $(this)
+  const file = modalButton.data('file')
+  deleteFile.val(file)
+  deleteFile.data('file', file)
 })
 
 refreshFiles.on('click', function () {
-    Helpers.refreshFiles(filesResult, $(this).data('target'))
-    if (queueSize) {
-        Helpers.updateQueueSize(queueSize)
-    }
+  Helpers.refreshFiles(filesResult, $(this).data('target'))
+  if (queueSize) {
+    Helpers.updateQueueSize(queueSize)
+  }
 })
 
 searchForm.on('shown.bs.modal', function () {
-    queryInput.focus()
+  queryInput.focus()
 })
 
 resultForm.on('click', '.rename', rename)
 logForm.on('click', '.rename', rename)
 
 deleteButton.on('click', function () {
-    const data = {
-        'file': deleteFile.data('file'),
-    }
+  const data = {
+    file: deleteFile.data('file')
+  }
 
-    Helpers.request('./api/v1/delete', data, function () {
-        Helpers.removeRow(modalButton)
-    })
+  Helpers.request('./api/v1/delete', data, function () {
+    Helpers.removeRow(modalButton)
+  })
 })
 
-function rename() {
-    const data = {
-        'file': $(this).data('file'),
-        'scene_id': $(this).data('scene-id'),
-    }
+function rename () {
+  const data = {
+    file: $(this).data('file'),
+    scene_id: $(this).data('scene-id')
+  }
 
-    Helpers.request('./api/v1/rename', data, function () {
-        Helpers.removeRow(modalButton)
-    })
+  Helpers.request('./api/v1/rename', data, function () {
+    Helpers.removeRow(modalButton)
+  })
 }
 
 Helpers.setTableSort(filesResult)
 
 if (queueSize) {
+  Helpers.updateQueueSize(queueSize)
+  setInterval(function () {
     Helpers.updateQueueSize(queueSize)
-    setInterval(function () {
-        Helpers.updateQueueSize(queueSize)
-    }, 5000)
+  }, 5000)
 }
