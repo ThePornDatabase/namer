4635270d2367da16e1bf46e66441cffb92f03912
DirtyRacer1337 <DirtyRacer@ya.ru>
2023-07-30 21:52:29 +0700

    Pipx (#203)
    * Add pipx support
    
    * Update database.py
    
    * Update dependencies
    
    * Minor changes
    
    * Remove webdriver_manager
    
    * Update dependencies

--- test/web/namer_web_test.py
+++ test/web/namer_web_test.py
@@ -2,17 +2,12 @@ import contextlib
 import os
 import unittest
 import warnings
-from pathlib import Path
 from platform import system
 
 import requests
 from selenium.webdriver import Chrome, ChromeOptions, Edge, EdgeOptions, Safari
-from selenium.webdriver.chrome.service import Service as ChromeService
-from selenium.webdriver.edge.service import Service as EdgeService
 from selenium.webdriver.remote.webdriver import WebDriver
 from selenium.webdriver.safari.service import Service as SafariService
-from webdriver_manager.chrome import ChromeDriverManager
-from webdriver_manager.microsoft import EdgeChromiumDriverManager
 
 from namer.configuration import NamerConfig
 from namer.watchdog import create_watcher
@@ -25,43 +20,31 @@ from test.web.parrot_webserver import ParrotWebServer
 
 def chrome_factory(debug: bool) -> WebDriver:
     options = ChromeOptions()
-    if (system() == 'Linux' and os.environ.get("DISPLAY") is None) or not debug:
-        options.headless = True
+    if (system() == 'Linux' and os.environ.get('DISPLAY') is None) or not debug:
+        options.add_argument('--headless')
     if system() != 'Windows' and os.geteuid() == 0:
-        options.add_argument("--no-sandbox")
+        options.add_argument('--no-sandbox')
 
-    webdriver_path = os.getenv('CHROMEWEBDRIVER', default=None)
-    if webdriver_path:
-        webdriver_path = Path(webdriver_path) / 'chromedriver'
-    else:
-        webdriver_path = ChromeDriverManager().install()
-
-    service = ChromeService(executable_path=str(webdriver_path), log_path=os.devnull)  # type: ignore
-    return Chrome(service=service, options=options)
+    return Chrome(options=options)
 
 
 def edge_factory(debug: bool) -> WebDriver:
     options = EdgeOptions()
-    if (system() == 'Linux' and os.environ.get("DISPLAY") is None) or not debug:
-        options.headless = True
+    if (system() == 'Linux' and os.environ.get('DISPLAY') is None) or not debug:
+        options.add_argument('--headless')
     if system() != 'Windows' and os.geteuid() == 0:
-        options.add_argument("--no-sandbox")
+        options.add_argument('--no-sandbox')
 
-    webdriver_path = os.getenv('EDGEWEBDRIVER', default=None)
-    if webdriver_path:
-        webdriver_path = Path(webdriver_path) / 'msedgedriver'
-    else:
-        webdriver_path = EdgeChromiumDriverManager().install()
+    webdriver = Edge(options=options)
 
-    service = EdgeService(executable_path=str(webdriver_path), log_path=os.devnull)  # type: ignore
-    webdriver = Edge(service=service, options=options)
     return webdriver
 
 
 def safari_factory(debug: bool) -> WebDriver:
     service = SafariService()
     with warnings.catch_warnings():
-        warnings.filterwarnings("ignore", category=DeprecationWarning)
+        warnings.filterwarnings('ignore', category=DeprecationWarning)
+
         return Safari(service=service)
 
 
@@ -71,7 +54,7 @@ def default_os_browser(debug: bool) -> WebDriver:
     # ci = ci_str.lower() == "true" if ci_str else False
     if name == 'Windows':  # and not ci:
         return edge_factory(debug)
-    # until github actions
+    # until GitHub actions
     # if name in ['Darwin', 'macOS']:
     #    return safari_factory(debug)
     return chrome_factory(debug)
@@ -80,7 +63,7 @@ def default_os_browser(debug: bool) -> WebDriver:
 @contextlib.contextmanager  # type: ignore
 def make_test_context(config: NamerConfig):
     with environment(config) as (tempdir, mock_tpdb, config), create_watcher(config) as watcher, default_os_browser(is_debugging()) as browser:
-        url = f"http://{config.host}:{watcher.get_web_port()}{config.web_root}/failed"
+        url = f'http://{config.host}:{watcher.get_web_port()}{config.web_root}/failed'
         browser.get(url)
         yield tempdir, watcher, browser, mock_tpdb
 
@@ -96,7 +79,7 @@ class UnitTestAsTheDefaultExecution(unittest.TestCase):
         """
         config = sample_config()
         config.web = True
-        config.web_root = "/namer"
+        config.web_root = '/namer'
         config.host = '127.0.0.1'
         config.port = 0
         config.allow_delete_files = True
@@ -111,9 +94,9 @@ class UnitTestAsTheDefaultExecution(unittest.TestCase):
                 .navigate_to().queue_page()
                 .navigate_to().failed_page()
                 .items()[0]
-                .file_name().is_equal_to("EvilAngel - 2022-01-03 - Carmela Clutch Fabulous Anal 3-Way!").on_success()
-                .file_extension().is_equal_to("MP4").on_success()
-                .show_log_modal().log_text().is_equal_to("No results found").on_success().close()
+                .file_name().is_equal_to('EvilAngel - 2022-01-03 - Carmela Clutch Fabulous Anal 3-Way!').on_success()
+                .file_extension().is_equal_to('MP4').on_success()
+                .show_log_modal().log_text().is_equal_to('No results found').on_success().close()
                 .items()[0]
                 .show_search_modal()
                 .search()
