143a7301fbac07cd5b221f852180471347300395
DirtyRacer <DirtyRacer@ya.ru>
2024-12-08 05:39:54 +0700

    update docker_build.sh

--- docker_build.sh
+++ docker_build.sh
@@ -1,12 +1,12 @@
 #!/bin/bash
+
+repo=theporndatabase
+version=$(cat pyproject.toml | grep -m1 "version = " | sed 's/.* = //' | sed 's/"//g' | tr -d '[:space:]')
+
 BUILD_DATE=$(date -u +'%Y-%m-%dT%H:%M:%SZ')
-CLEAN=$(git diff-index --quiet HEAD; echo $?)
-version=$(cat pyproject.toml | grep -m1 "version = " | sed 's/.* = //' | sed 's/"//g')
-#if [[ "${CLEAN}" == "0" ]]; then
-  export BUILD_DATE=$(date -u +'%Y-%m-%dT%H:%M:%SZ')
-  export GIT_HASH=$(git rev-parse --verify HEAD)
-  export repo=theporndatabase
-  docker build . --build-arg "BUILD_DATE=${BUILD_DATE}" --build-arg "GIT_HASH=${GIT_HASH}" --build-arg "PROJECT_VERSION=${version}" -t "ghcr.io/${repo}/namer:${version}" -t "ghcr.io/${repo}/namer:latest"
-#else
-#  echo Not building for dirty code.
-#fi
+export BUILD_DATE
+
+GIT_HASH=$(git rev-parse --verify HEAD)
+export GIT_HASH
+
+docker build . --build-arg "BUILD_DATE=${BUILD_DATE}" --build-arg "GIT_HASH=${GIT_HASH}" --build-arg "PROJECT_VERSION=${version}" -t "ghcr.io/${repo}/namer:${version}" -t "ghcr.io/${repo}/namer:latest"
